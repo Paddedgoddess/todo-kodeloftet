4c209902aae65472b0260410803a6dcfed57ef9e
Paddedgoddess <goddesspadded@gmail.com>
2024-04-18 14:41:22 +0200

    appearance_and_functionality_added

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ function addTodo() {
     event.preventDefault();
     console.log("Is button clicked?");
 
-//const er å erklære en variabel
-// taskInputData er et variabel navn
-//= å erklære en verdi som skal lagres
-// document referer til HTML dokument
-//.queryselector() henter noe fra HTML
-// .value er at vi henter innholdet til elementet
-// ; betyr full stopp. denne linjen med kode er ferdig
-
 
 //fetches text from textbox into java
     const inputTaskData = document.querySelector("#inputTask").value;
@@ -21,20 +13,41 @@ function addTodo() {
     const newListItem = document.createElement("li");
     console.log(newListItem);
 
-//
-    newListItem.textContent = inputTaskData;
 
-//
+const checkbox = document.createElement("input");
+checkbox.type = "checkbox";
+
+newListItem.appendChild(checkbox);
+
+    newListItem.appendChild(document.createTextNode(inputTaskData));
+
+    newListItem.classList.add("listTaskItem");
+    
     const listTask = document.querySelector("#listTask");
     console.log(listTask);
+    listTask.appendChild(newListItem);
+
 
 //
-    listTask.appendChild(newListItem);
-    console.log(listTask)
+document.querySelector("#inputTask").value = "";
+
+document.querySelector("#inputTask").focus();
 }
 
 
-const buttonTask = document.querySelector("#buttonTask");
-console.log(buttonTask);
 
+function removeCheckedItems() {
+    const checkedItems = document.querySelectorAll("#listTask input[type='checkbox']:checked");
+
+
+    checkedItems.forEach(item => {
+        item.parentNode.remove();
+    });
+}
+
+const buttonTask = document.querySelector("#buttonTask");
 buttonTask.addEventListener("click", addTodo);
+
+const buttonRemove = document.querySelector("#removeCheckedItems");
+buttonRemove.addEventListener("click", removeCheckedItems);
+console.log(addEventListener)
